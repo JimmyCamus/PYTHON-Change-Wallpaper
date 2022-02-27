7c0f22e79d29e2ae9203c5cd956849d758400a5a
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-02-27 00:57:20 -0300

    v2.0.1 -> little fix in main

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from utils.handle_db import read_data
 from utils.menu_manager import menu_navigation
 
 
-def main():
-    change_wallpaper()
-
-
 if __name__ == '__main__':
     container = read_data()
     menu_navigation(container)
